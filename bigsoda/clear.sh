a8b9ce75d45af966d700bf6730bfa99ffac56e7f
Damian Sromek <damian.sromek@gmail.com>
2015-09-11 13:57:44 +0200

    Remove clear.sh duplicate

--- bigsoda/clear.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# WARNING!!!
-# Clears all Docker's images and containers
-
-# Delete all containers
-docker rm -f $(docker ps -a -q)
-# Delete all images
-docker rmi -f $(docker images -q)
